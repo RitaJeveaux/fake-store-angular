879ecd1e5c2e03e0da2797fd6d7f6f1dbff7b35f
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-01 10:55:17 -0300

    checkout funcionando

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomValidators } from '../../validators/custom.validators';
